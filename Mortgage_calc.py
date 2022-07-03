fe3548f3b49a65668a83a8e88702bec34a42ed0d
Lukas <ldolidze@purdue.edu>
2022-07-03 16:32:24 +0400

    Reworked to add classes and methods

--- Mortgage_calc.py
+++ Mortgage_calc.py
@@ -1,9 +1,16 @@
-print("What is your down payment?")
-down = int(input())
-print("For how long do you plan to pay the mortgage?")
-months = int(input())
-print("What is the home value in USD($)?")
-value = int(input())
-r = 0.065 / 12
-monthly = (r * down) / (1 - (1 + r)**(-months)) 
-print(monthly)
+class Mortgage():
+    def __init__(self, name, principal, months):
+        self.name = name
+        self.principal = principal
+        self.months = months
+
+    def calc_mortgage(self,principal, months, r = 0.065 / 12):
+        monthly = (r * principal) / (1 - (1 + r)**(-months)) 
+        return "{name}, your monthly payments will come out to {monthly:.2f}$".format(name = name, monthly = monthly)
+
+name = input("What is your name?\n")
+principal = int(input("What is your down payment?\n"))
+months = int(input("For how many months do you plan to pay the mortgage?\n"))
+
+mortgage = Mortgage(name, principal, months)
+print(mortgage.calc_mortgage(principal, months))
