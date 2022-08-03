593e95db66d311f0f7aba3b3a1b397ffef955c54
Lukas <ldolidze@purdue.edu>
2022-08-03 10:06:04 +0400

    test

--- kevin.py
+++ kevin.py
@@ -1,3 +1,3 @@
 import pandas as pd
-df=pd.read_csv(r'D:/Python shit/homework/2022 08 02 Turbidity Assay - Result sheet.csv')
+df=pd.read_csv(https://raw.githubusercontent.com/somethington/homework/kevinttest/2022%2008%2002%20Turbidity%20Assay%20-%20Result%20sheet.csv)
 print(df)
