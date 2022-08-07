a5e07cc3b7795a62745c3a7c38d39f5130377680
Lukas <ldolidze@purdue.edu>
2022-08-07 09:27:23 +0400

    finds mean for columns of interest

--- kevin.py
+++ kevin.py
@@ -26,18 +26,22 @@ for i in range(1,13):
 index_advance = []
 for i in range(2, len(columns_of_interest_dsub), 3):
     index_advance.append(i)
-print(index_advance)
 
 index_c = 0
 index_d = 0
+lst_d_sub_columns = []
 while index_c < len(columns_of_interest_dsub): #subtracts D[x] from respective group of samples
     source[str(columns_of_interest_dsub[index_c]) + "-D"] = source[columns_of_interest_dsub[index_c]] - source[d_list[index_d]]
+    lst_d_sub_columns.append(str(columns_of_interest_dsub[index_c]) + "-D")
     if index_c in index_advance:
         index_d += 1
     if index_d == len(d_list):
         break
     index_c += 1
     
+for i in range(0, len(lst_d_sub_columns)):
+    source[lst_d_sub_columns[i] + " mean"] = source[lst_d_sub_columns[i]].mean()
+
 source.to_csv("curiosity.csv")
 
                 
