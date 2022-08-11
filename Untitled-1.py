b9c5bd2c980643c6c49100c6ac0bc27943ad4e8e
somethington <ldolidze@purdue.edu>
2022-08-12 01:05:48 +0400

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,9 +0,0 @@
-i = 0
-magazine = "aab"
-ransomNote = "aa"
-while len(magazine) > 0:
-    for letter in magazine:
-        if letter in ransomNote:
-            magazine -= letter
-            i += 1
-len(ransomNote) == i
